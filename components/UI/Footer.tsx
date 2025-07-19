1dd1f1ab795ddbcf392cd809071bb20d86345d99
Apologle <otl5994@naver.com>
2025-07-19 21:46:43 +0900

    feat : 글로벌 스타일 추가 <a> 태그에 대한 기본 스타일 제거

--- components/UI/Footer.tsx
+++ components/UI/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import messageIcon from '@/assets/img/messageIcon.png';
 import facebookIcon from '@/assets/img/facebookIcon.png';
 import instagramIcon from '@/assets/img/instagramIcon.png';
@@ -8,17 +9,30 @@ export default function Footer() {
 	return (
 		<>
 			<div className={styles.Footer}>
-				<div>©codeit - 2023</div>
-				<div>
-					<div>Privacy Policy</div>
-					<div>FAQ</div>
+				<div className={styles.FooterContents}>
+					<div>©codeit - 2023</div>
+					<div className={styles.Policies}>
+						<Link href="/">Privacy Policy</Link>
+						<Link href="/">FAQ</Link>
+					</div>
+					<div className={styles.ImgDiv}>
+						<p>
+							<a href="https://www.google.com" target="_blank">
+								<Image src={messageIcon} alt="메세지 아이콘" />
+							</a>
+						</p>
+						<p>
+							<a href="https://www.facebook.com" target="_blank">
+								<Image src={facebookIcon} alt="페이스북 아이콘" />
+							</a>
+						</p>
+						<p>
+							<a href="https://www.instagram.com" target="_blank">
+								<Image src={instagramIcon} alt="인스타그램 아이콘" />
+							</a>
+						</p>
+					</div>
 				</div>
-				<div>
-					<Image src={messageIcon} alt="메세지 아이콘" />
-					<Image src={facebookIcon} alt="페이스북 아이콘" />
-					<Image src={instagramIcon} alt="인스타그램 아이콘" />
-				</div>
-				<button>dfdf</button>
 			</div>
 		</>
 	);
