227aff1daf11777f0da96df8bf2046fdf360e6da
Apologle <otl5994@naver.com>
2025-07-19 20:59:26 +0900

    [최민준] feat: ul,li,button 스타일 없애는 글로벌 스타일 추가

--- components/UI/Footer.tsx
+++ components/UI/Footer.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image';
+import messageIcon from '@/assets/img/messageIcon.png';
+import facebookIcon from '@/assets/img/facebookIcon.png';
+import instagramIcon from '@/assets/img/instagramIcon.png';
+import styles from './Footer.module.css';
+
 export default function Footer() {
-	return <></>;
+	return (
+		<>
+			<div className={styles.Footer}>
+				<div>©codeit - 2023</div>
+				<div>
+					<div>Privacy Policy</div>
+					<div>FAQ</div>
+				</div>
+				<div>
+					<Image src={messageIcon} alt="메세지 아이콘" />
+					<Image src={facebookIcon} alt="페이스북 아이콘" />
+					<Image src={instagramIcon} alt="인스타그램 아이콘" />
+				</div>
+				<li>
+					<ul>sd</ul>
+				</li>
+			</div>
+		</>
+	);
 }
