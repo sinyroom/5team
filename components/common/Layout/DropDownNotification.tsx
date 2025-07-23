b4f2ff8632f6d6c38830d14a45b6aaa8c87914ee
Apologle <otl5994@naver.com>
2025-07-24 01:02:22 +0900

    [최민준] fix: 민경님이 만들어주신 모달 컴포넌트로 알림 모달 창 수정.

--- components/common/Layout/DropDownNotification.tsx
+++ components/common/Layout/DropDownNotification.tsx
@@ -2,8 +2,29 @@ import React, { useState } from 'react';
 
 import Notification from '@/assets/img/notification.svg';
 import styles from './DropDownNotification.module.css';
+import NotificationCard from '@/components/Modal/Notification/Notification.tsx';
+
 export default function DropDownMenu() {
 	const [dropdownVisible, setDropdownVisible] = useState(false); //visible 상태
+
+	const mok = [
+		{
+			id: 1,
+			store: '카페 굿모닝',
+			date: '2024-07-24',
+			time: '10:30',
+			status: '승인',
+			ago: '1시간 전',
+		},
+		{
+			id: 1,
+			store: '카페 굿모닝',
+			date: '2024-07-24',
+			time: '10:30',
+			status: '승인',
+			ago: '1시간 전',
+		},
+	];
 
 	const toggleDropdown = () => {
 		setDropdownVisible(prev => !prev);
@@ -16,8 +37,8 @@ export default function DropDownMenu() {
 			</button>
 
 			{dropdownVisible && (
-				<div className={styles.dropDownMenu}>
-					<div>알림</div>
+				<div className={styles.notificationLocation}>
+					<NotificationCard notifications={mok} onClose={toggleDropdown} />
 				</div>
 			)}
 		</>
