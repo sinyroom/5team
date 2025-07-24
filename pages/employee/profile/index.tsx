7004ae745491154ee1fb0bcc0f67905ff4bf8919
Apologle <otl5994@naver.com>
2025-07-25 02:21:51 +0900

    [최민준] feat : employee 상세 페이지 작성 + fix : pathIcon svg 파일 기본 가로세로(svg 자체 설정) 없앱니다. => svg 자체에서 가로세로 값 줘버리면 그 값이 우선적으로 처리되기때문에 크기를 변경할 수 없습니다.

--- pages/employee/profile/index.tsx
+++ pages/employee/profile/index.tsx
@@ -4,24 +4,36 @@ import { useState } from 'react';
 //style
 import styles from './profile.module.css';
 
+//image
+import PathIcon from '@/assets/img/pathIcon.svg';
+import SmartPhoneIcon from '@/assets/img/smartPhoneIcon.svg';
+
 //employee/profile
 export default function ProfilePage() {
+	const [applyList, setApplyList] = useState(false); // 신청 내역 여부입니다. default, false.
+
 	return (
 		<>
-			<div>
-				<div>내 프로필</div>
-				<div>
+			<div className={styles.profileBlock}>
+				<div className={styles.profileTitle}>내 프로필</div>
+				<div className={styles.profileDetailed}>
 					<div>
-						<div>이름</div>
-						<div>홍길동</div>
-						<div>010-0000-0000</div>
-						<div>선호 지역: 서울시 도봉구</div>
-						<div>열심히 일 하겠습니다.</div>
+						<div id={styles.name}>이름</div>
+						<div id={styles.profileName}>홍길동</div>
+						<div id={styles.phoneNumber}>
+							<SmartPhoneIcon style={{ width: '20px', height: '20px' }} />
+							010-0000-0000
+						</div>
+						<div id={styles.preferLocation}>
+							<PathIcon style={{ objectFit: 'contain', width: '20px', height: '20px' }} />
+							선호 지역: 서울시 도봉구
+						</div>
+						<div id={styles.profileIntroduce}>열심히 일 하겠습니다.</div>
 					</div>
-					<div>(button) 편집하기</div>
+					<div> 편집하기</div>
 				</div>
 			</div>
-			<div>신청 내역</div>
+			<div className={styles.applyListBlock}>신청 내역</div>
 		</>
 	);
 }
