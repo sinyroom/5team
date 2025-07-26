c9b06e20ff19fd53961752f390663c162d044cc6
sinyroom <wldmsdl7913@naver.com>
2025-07-26 11:14:31 +0900

    [문지영] feat: 유저 정보 조회 및 수정 api 연결

--- pages/employee/profile/create.tsx
+++ pages/employee/profile/create.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import buttonStyle from '@/components/common/BaseButton/BaseButton.module.css';
 import styles from './create.module.css';
@@ -7,17 +7,105 @@ import { TextInput } from '@/components/common/inputs/TextInput';
 import { DropdownInput } from '@/components/common/inputs/DropdownInput';
 import { TextareaInput } from '@/components/common/inputs/TextareaInput';
 import Alert from '@/components/Modal/Alert/Alert';
+import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+import axiosInstance from '@/api/settings/axiosInstance';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import { getUser } from '@/api/users/getUser';
+import { updateUser } from '@/api/users/updateUser';
 
-const Create = () => {
-	const [isAlertOpen, setIsAlertOpen] = useState(false);
+export const getServerSideProps: GetServerSideProps = async context => {
+	const { userId, token } = context.req.cookies;
+
+	// 테스트용 쿠키 값 삽입
+	// let { userId, token } = context.req.cookies;
+	// userId = '7d36a348-c505-4452-8f29-a6c1fa1d01c6';
+	// token =
+	// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3ZDM2YTM0OC1jNTA1LTQ0NTItOGYyOS1hNmMxZmExZDAxYzYiLCJpYXQiOjE3NTMzNDgzODJ9.G6VJdK55gx8jRPu-eAD0nEdFxCfmv4NRgdniJYffBUo';
+
+	// 디버깅용 코드
+	// console.log('서버사이드 쿠키:', context.req.cookies);
+	// console.log('사용할 userId:', userId);
+	// console.log('사용할 token:', token);
+
+	if (!userId || !token) {
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			},
+		};
+	}
+	try {
+		const userData = await getUser(userId, token);
+		return {
+			props: {
+				userData,
+			},
+		};
+	} catch (error) {
+		return {
+			props: {
+				userData: null,
+			},
+		};
+	}
+};
+
+const Create = ({ userData }: { userData: any }) => {
 	const [name, setName] = useState('');
 	const [phone, setPhone] = useState('');
 	const [address, setAddress] = useState('');
 	const [bio, setBio] = useState('');
+	const [isAlertOpen, setIsAlertOpen] = useState(false);
+	const [alertMessage, setAlertMessage] = useState('');
 
-	const handleSubmit = (e: React.FormEvent) => {
+	const router = useRouter();
+
+	useEffect(() => {
+		const userIdCookie = Cookies.get('userId');
+		const tokenCookie = Cookies.get('token');
+		// 테스트용 코드
+		// if (!userIdCookie || !tokenCookie) {
+		// 	Cookies.set('userId', '7d36a348-c505-4452-8f29-a6c1fa1d01c6', {
+		// 		path: '/',
+		// 		sameSite: 'lax',
+		// 		secure: false,
+		// 	});
+		// 	Cookies.set(
+		// 		'token',
+		// 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3ZDM2YTM0OC1jNTA1LTQ0NTItOGYyOS1hNmMxZmExZDAxYzYiLCJpYXQiOjE3NTMzNDgzODJ9.G6VJdK55gx8jRPu-eAD0nEdFxCfmv4NRgdniJYffBUo',
+		// 		{ path: '/', sameSite: 'lax', secure: false }
+		// 	);
+		// }
+
+		const userInfo = userData.item;
+		if (userData) {
+			setName(userInfo.name || '');
+			setPhone(userInfo.phone || '');
+			setAddress(userInfo.address || '');
+			setBio(userInfo.bio || '');
+		}
+	}, []);
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		const userId = Cookies.get('userId');
+		const token = Cookies.get('token');
 		e.preventDefault();
-		setIsAlertOpen(true);
+		try {
+			await updateUser(userId!, token!, { name, phone, address, bio });
+
+			setAlertMessage('등록이 완료되었습니다.');
+			setIsAlertOpen(true);
+		} catch {
+			setAlertMessage('등록에 실패했습니다.');
+			setIsAlertOpen(true);
+		}
+	};
+
+	const handleAlertConfirm = () => {
+		router.push('/profile');
 	};
 
 	const handleAlertClose = () => {
@@ -87,8 +175,8 @@ const Create = () => {
 
 				<Alert
 					isOpen={isAlertOpen}
-					message="등록이 완료되었습니다."
-					onConfirm={handleAlertClose}
+					message={alertMessage}
+					onConfirm={handleAlertConfirm}
 					onClose={handleAlertClose}
 				></Alert>
 			</div>
