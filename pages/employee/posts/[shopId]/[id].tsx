8b7d424ff74edf8b01d82c33c84088afcddea8c7
sinyroom <wldmsdl7913@naver.com>
2025-07-31 15:51:17 +0900

    [문지영] feat: 프로필 미등록 시  모달 실행

--- pages/employee/posts/[shopId]/[id].tsx
+++ pages/employee/posts/[shopId]/[id].tsx
@@ -5,9 +5,15 @@ import { getShopById } from '@/api/users/getShopById';
 import { GetNoticeResponse, Notice } from '@/types/userNotice';
 import { Shop } from '@/types';
 import { fetchNoticeList } from '@/api/users/getNotice';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SmallNoticePoastCard from '@/components/common/NoticePostCard/SmallNoticePoastCard';
-import styles from './detailPost.module.css';
+import styles from './postDetail.module.css';
+import NoticePostCard from '@/components/common/NoticePostCard';
+import BtnStyles from '@/components/common/BaseButton/BaseButton.module.css';
+import { useUserContext } from '@/contexts/auth-context';
+import { getUser } from '@/api/users/getUser';
+import Confirm from '@/components/Modal/Confirm/Confirm';
+import useModal from '@/hooks/useModal';
 
 interface Props {
 	notice: Notice | null;
@@ -46,22 +52,63 @@ export const getServerSideProps: GetServerSideProps = async context => {
 
 const PostDetailPage = ({ viewedNotices, notice, shop }: Props) => {
 	const [newlyNotices, setNewlyNotices] = useState(viewedNotices.items);
+	const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+	const [alertMessage, setAlertMessage] = useState('');
 
 	const router = useRouter();
+	const { user } = useUserContext();
+	const confirmModal = useModal();
 
 	if (!notice || !shop) return <p>존재하지 않는 공고입니다.</p>;
 
+	const handleClick = async () => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			setAlertMessage('로그인이 필요합니다');
+			setIsConfirmOpen(true);
+			return;
+		}
+
+		try {
+			const profileRes = await getUser(user!.id, token);
+			const { name, phone, address, bio } = profileRes.item;
+
+			if (!name || !phone || !address || !bio) {
+				setAlertMessage('내 프로필을 먼저 등록해 주세요');
+				setIsConfirmOpen(true);
+				return;
+			}
+
+			console.log('신청 처리');
+		} catch (err) {
+			console.error('프로필 조회 실패', err);
+			alert('프로필 정보를 불러오는 중 오류가 발생했습니다.');
+		}
+	};
+
 	return (
 		<div>
-			<h1>{shop.name}</h1>
-			<p>{notice.description}</p>
-			<p>시급: {notice.hourlyPay}원</p>
-			<p>근무지: {shop.address1}</p>
+			<div>
+				<NoticePostCard notice={notice}>
+					<button className={`${styles.button} ${BtnStyles.red}`} onClick={handleClick}>
+						신청하기
+					</button>
+				</NoticePostCard>
+			</div>
 			<div className={styles.newlyPost}>
 				{newlyNotices.map(({ item }: { item: Notice }, idx: number) => (
 					<SmallNoticePoastCard key={idx} notice={item} />
 				))}
 			</div>
+
+			{isConfirmOpen && (
+				<Confirm
+					message={alertMessage}
+					isOpen={isConfirmOpen}
+					onClose={() => setIsConfirmOpen(false)}
+					onConfirm={() => setIsConfirmOpen(false)}
+				/>
+			)}
 		</div>
 	);
 };
