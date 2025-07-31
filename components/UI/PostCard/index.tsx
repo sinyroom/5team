661b4c335f84c22483044960cbddc9207816aaea
Seonghyun Hong <shfo18@naver.com>
2025-07-31 13:33:39 +0900

    [홍성현] feat: 공고상세 NoticePostCard 컴포넌트 추가 & 반응형 수정

--- components/UI/PostCard/index.tsx
+++ components/UI/PostCard/index.tsx
@@ -13,26 +13,33 @@ const Post = ({ shop }: PostCardProps) => {
 	return (
 		<ul className={styles.post}>
 			<li className={styles.postImageUrl}>{shop?.imageUrl}</li>
-			<li className={styles.postCategory}>{shop?.category}</li>
-			<li className={styles.postName}>{shop?.name}</li>
-			<li className={styles.postAddress1}>
-				<PathIcon width={16} height={16} />
-				{shop?.address1}
-			</li>
-			<li className={styles.postdescription}>{shop?.description}</li>
-			<li className={styles.buttonGruop}>
-				<BaseButton
-					children={'편집하기'}
-					onClick={() => router.push('/owner/store/edit')}
-					size={'noneSize'}
-					color={'white'}
-				/>
-				<BaseButton
-					children={'공고 등록하기'}
-					onClick={() => router.push('/owner/recruit/create')}
-					size={'noneSize'}
-				/>
-			</li>
+			<div className={styles.responsiveDesign}>
+				<div className={styles.responsiveDesignContents}>
+					<li className={styles.postCategory}>{shop?.category}</li>
+					<li className={styles.postName}>{shop?.name}</li>
+					<li className={styles.postAddress1}>
+						<PathIcon width={16} height={16} />
+						{shop?.address1}
+					</li>
+					<li className={styles.postdescription}>{shop?.description}</li>
+				</div>
+				<div className={styles.responsiveDesignButton}>
+					<li className={styles.buttonGruop}>
+						<BaseButton
+							children={'편집하기'}
+							onClick={() => router.push('/owner/store/edit')}
+							size={'noneSize'}
+							color={'white'}
+						/>
+						<BaseButton
+							children={'공고 등록하기'}
+							onClick={() => router.push('/owner/recruit/create')}
+							size={'noneSize'}
+							color={'red'}
+						/>
+					</li>
+				</div>
+			</div>
 		</ul>
 	);
 };
