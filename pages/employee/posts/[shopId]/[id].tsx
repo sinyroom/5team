c2b0648a143ad1885c8be648bf71b98cd0fbf542
sinyroom <wldmsdl7913@naver.com>
2025-07-31 13:48:20 +0900

    [문지영] feat: 공고 리스트 컴포넌트 불러오기

--- pages/employee/posts/[shopId]/[id].tsx
+++ pages/employee/posts/[shopId]/[id].tsx
@@ -2,32 +2,41 @@ import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { getNoticeId } from '@/api/getNoticeId';
 import { getShopById } from '@/api/users/getShopById';
-import { Notice } from '@/types/userNotice';
+import { GetNoticeResponse, Notice } from '@/types/userNotice';
 import { Shop } from '@/types';
+import { fetchNoticeList } from '@/api/users/getNotice';
+import { useState } from 'react';
+import SmallNoticePoastCard from '@/components/common/NoticePostCard/SmallNoticePoastCard';
+import styles from './detailPost.module.css';
 
 interface Props {
 	notice: Notice | null;
 	shop: Shop | null;
+	viewedNotices: GetNoticeResponse;
 }
 
 export const getServerSideProps: GetServerSideProps = async context => {
 	const { shopId, id: noticeId } = context.params!;
 	try {
-		const res = await getNoticeId(shopId as string, noticeId as string); // ✅ id 기반 공고 fetch
+		const res = await getNoticeId(shopId as string, noticeId as string);
 		const notice = res.item;
 
 		let shop = null;
+		const viewedNotices = await fetchNoticeList({
+			offset: 0,
+			limit: 6,
+		});
 		if (notice?.shop?.item) {
 			shop = notice.shop.item;
 		} else {
 			const shopRes = await getShopById(shopId as string);
 			shop = shopRes.item;
 		}
-
 		return {
 			props: {
 				notice,
 				shop,
+				viewedNotices,
 			},
 		};
 	} catch (error) {
@@ -35,7 +44,9 @@ export const getServerSideProps: GetServerSideProps = async context => {
 	}
 };
 
-const PostDetailPage = ({ notice, shop }: Props) => {
+const PostDetailPage = ({ viewedNotices, notice, shop }: Props) => {
+	const [newlyNotices, setNewlyNotices] = useState(viewedNotices.items);
+
 	const router = useRouter();
 
 	if (!notice || !shop) return <p>존재하지 않는 공고입니다.</p>;
@@ -46,6 +57,11 @@ const PostDetailPage = ({ notice, shop }: Props) => {
 			<p>{notice.description}</p>
 			<p>시급: {notice.hourlyPay}원</p>
 			<p>근무지: {shop.address1}</p>
+			<div className={styles.newlyPost}>
+				{newlyNotices.map(({ item }: { item: Notice }, idx: number) => (
+					<SmallNoticePoastCard key={idx} notice={item} />
+				))}
+			</div>
 		</div>
 	);
 };
